7212ad6d3ba4eccc0ab575be7ea951c273e70bd8
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-11 17:20:43 -0300

    created seeds for new article MVC pattern

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning up database'
+Article.destroy_all
+puts 'database is clean'
+
+puts 'creating articles'
+10.times do
+  article = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Twitter.status
+  )
+  puts "article #{article.title} is created"
+end
+
+puts 'articles created'
